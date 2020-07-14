274256e1492b57762e82d29d2a020acc70514597
youngmiin <happistar09@gmail.com>
2020-07-14 13:00:10 +0900

    installation upload

--- Installation/src/main/java/ipTVShopProject/InstallationController.java
+++ Installation/src/main/java/ipTVShopProject/InstallationController.java
@@ -1,16 +1,41 @@
 package ipTVShopProject;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
  @RestController
  public class InstallationController {
+  @Autowired
+  InstallationRepository installationRepository;
+
+  @RequestMapping(method=RequestMethod.GET, path="/installations")
+  public String installationCancellation(@RequestBody Installation installation) {
+
+   Installation installationCancel = installationRepository.findByOrderId(installation.getOrderId());
+   if (installationCancel.getStatus() == "INSTALLCOMPLETED") {
+    return "NotAccepted";
+   } else {
+    return "Accepted";
+   }
+  }
+
+   @RequestMapping(method=RequestMethod.PATCH, path="/installations")
+   public void installationCompletion(@RequestParam (value="orderId", required=false, defaultValue="0") Long orderId) {
+
+    Installation installationCompl = installationRepository.findByOrderId(orderId);
+    installationCompl.setStatus("INSTALLCOMPLETED");
+    SimpleDateFormat defaultSimpleDateFormat = new SimpleDateFormat("YYYYMMddHHmmss");
+    String today = defaultSimpleDateFormat.format(new Date());
+    installationCompl.setInstallCompleteDate(today);
+    installationRepository.save(installationCompl);
+
+   }
 
 
  }
--- Installation/src/main/java/ipTVShopProject/PolicyHandler.java
+++ Installation/src/main/java/ipTVShopProject/PolicyHandler.java
@@ -8,13 +8,29 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Service
 public class PolicyHandler{
-    
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @Autowired
+    InstallationRepository installationRepository;
+
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverOrderAccepted_InstallationRequest(@Payload OrderAccepted orderAccepted){
 
         if(orderAccepted.isMe()){
+            Installation installationAccept = new Installation();
+            installationAccept.setStatus("INSTALLATIONACCEPTED");
+            SimpleDateFormat defaultSimpleDateFormat = new SimpleDateFormat("YYYYMMddHHmmss");
+            String today = defaultSimpleDateFormat.format(new Date());
+            installationAccept.setInstallReservationDate(today);
+            installationRepository.save(installationAccept);
             System.out.println("##### listener InstallationRequest : " + orderAccepted.toJson());
         }
     }
