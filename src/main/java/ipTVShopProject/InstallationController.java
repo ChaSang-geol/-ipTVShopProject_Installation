d1a7be11999aea74aad0edbfa5c7af97451f65d9
Cha, Sang-geol <56263370+ChaSang-geol@users.noreply.github.com>
2020-07-14 20:16:56 +0900

    Add files via upload

--- src/main/java/ipTVShopProject/InstallationController.java
+++ src/main/java/ipTVShopProject/InstallationController.java
@@ -1,39 +1,39 @@
-package ipTVShopProject;
+ package ipTVShopProject;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+         import org.springframework.beans.factory.annotation.Autowired;
+         import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+         import javax.servlet.http.HttpServletRequest;
+         import javax.servlet.http.HttpServletResponse;
+         import java.text.SimpleDateFormat;
+         import java.util.Date;
+         import java.util.List;
 
- @RestController
- public class InstallationController {
-  @Autowired
-  InstallationRepository installationRepository;
+@RestController
+public class InstallationController {
+    @Autowired
+    InstallationRepository installationRepository;
 
-  @RequestMapping(method=RequestMethod.POST, path="/installations")
-  public void installationCancellation(@RequestBody Installation installation) {
+    @RequestMapping(method=RequestMethod.POST, path="/installations")
+    public void installationCancellation(@RequestBody Installation installation) {
 
-   Installation installationCancel = installationRepository.findByOrderId(installation.getOrderId());
-   installationCancel.setStatus("INSTALLATIONCANCELED");
-   installationRepository.save(installationCancel);
+        Installation installationCancel = installationRepository.findByOrderId(installation.getOrderId());
+        installationCancel.setStatus("INSTALLATIONCANCELED");
+        installationRepository.save(installationCancel);
 
-  }
+    }
 
-   @RequestMapping(method=RequestMethod.PATCH, path="/installations")
-   public void installationCompletion(@RequestParam (value="orderId", required=false, defaultValue="0") Long orderId) {
+    @RequestMapping(method=RequestMethod.PATCH, path="/installations")
+    public void installationCompletion(@RequestParam (value="orderId", required=false, defaultValue="0") Long orderId) {
 
-       Installation installationCompl = installationRepository.findByOrderId(orderId);
-       installationCompl.setStatus("INSTALLCOMPLETED");
-       SimpleDateFormat defaultSimpleDateFormat = new SimpleDateFormat("YYYYMMddHHmmss");
-       String today = defaultSimpleDateFormat.format(new Date());
-       installationCompl.setInstallCompleteDate(today);
-       installationRepository.save(installationCompl);
+        Installation installationCompl = installationRepository.findByOrderId(orderId);
+        installationCompl.setStatus("INSTALLCOMPLETED");
+        SimpleDateFormat defaultSimpleDateFormat = new SimpleDateFormat("YYYYMMddHHmmss");
+        String today = defaultSimpleDateFormat.format(new Date());
+        installationCompl.setInstallCompleteDate(today);
+        installationRepository.save(installationCompl);
 
-   }
+    }
 
 
- }
+}
